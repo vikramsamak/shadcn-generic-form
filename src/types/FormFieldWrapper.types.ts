3a943cc765e2bd52f296200b8c25ae0a2bdcc236
vikramsamak <vikramsamak02@gmail.com>
2025-03-02 09:24:53 +0530

    refactor: update FormFieldWrapperProps to support custom event types

--- src/types/FormFieldWrapper.types.ts
+++ src/types/FormFieldWrapper.types.ts
@@ -2,6 +2,6 @@ import { Control, FieldValues } from "react-hook-form";
 import { FormFieldConfig } from "./FormField.types";
 
 export interface FormFieldWrapperProps<T extends FieldValues> {
-  formField: FormFieldConfig<T>;
+  formField: FormFieldConfig<T, Event>;
   control: Control<T>;
 }
