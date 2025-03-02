ceb7e9d14bf467bb14921a44bc15af9c9294d5f5
vikramsamak <vikramsamak02@gmail.com>
2025-03-02 11:10:45 +0530

    feat: enhance Header component with GitHub link button and improved layout

--- example/src/components/header.tsx
+++ example/src/components/header.tsx
@@ -1,10 +1,23 @@
+import { Github } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
 
 export default function Header() {
   return (
     <header className="flex items-center justify-between p-4 border-b">
       <div className="text-xl font-bold">ShadCN Generic Form</div>
-      <ModeToggle />
+
+      <div className="flex items-center gap-4">
+        <a
+          href="https://github.com/vikramsamak/shadcn-generic-form"
+          target="_blank"
+        >
+          <Button variant="ghost" size="icon" className="rounded-full">
+            <Github />
+          </Button>
+        </a>
+        <ModeToggle />
+      </div>
     </header>
   );
 }
