5b2c62372038d5fabda8943c522af1d7e8131df7
vikramsamak <vikramsamak02@gmail.com>
2025-03-02 23:52:33 +0530

    feat: integrate GenericFormCard into App component and enhance DatePicker with props for better date selection handling

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,14 @@
-import Header from "./components/header";
 import { ScrollArea } from "./components/ui/scroll-area";
+import Header from "./components/header";
+import GenericFormCard from "./components/generic-form-card";
 
 function App() {
   return (
     <ScrollArea className="flex flex-col h-screen">
       <Header />
-      <main></main>
+      <main className="flex justify-center items-center min-h-[calc(100vh-4rem)] p-4">
+        <GenericFormCard />
+      </main>
     </ScrollArea>
   );
 }
